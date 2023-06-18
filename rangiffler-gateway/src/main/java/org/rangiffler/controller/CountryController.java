0d26936f2ec9bf15a501802cf2e76871e917ba4c
Denis Shokin <DShokin@alfabank.ru>
2023-06-18 21:32:03 +0300

    step 3: add gateway-service template

---
+++ rangiffler-gateway/src/main/java/org/rangiffler/controller/CountryController.java
@@ -0,0 +1,26 @@
+package org.rangiffler.controller;
+
+import org.rangiffler.model.CountryJson;
+import org.rangiffler.service.api.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CountryController {
+
+  private final CountryService countryService;
+
+  @Autowired
+  public CountryController(CountryService countryService) {
+    this.countryService = countryService;
+  }
+
+  @GetMapping("/countries")
+  public List<CountryJson> getAllCountries() {
+    return countryService.getAllCountries();
+  }
+
+}
---
+++ rangiffler-gateway/src/main/java/org/rangiffler/controller/UserController.java
@@ -0,0 +1,72 @@
+package org.rangiffler.controller;
+
+import org.rangiffler.model.UserJson;
+import org.rangiffler.service.api.UserApiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private final UserApiService userApiService;
+
+    @Autowired
+    public UserController(UserApiService userApiService) {
+        this.userApiService = userApiService;
+    }
+
+    @GetMapping("/users")
+    public List<UserJson> getAllUsers() {
+        return userApiService.getAllUsers();
+    }
+
+    @GetMapping("/currentUser")
+    public UserJson getCurrentUser(@AuthenticationPrincipal Jwt principal) {
+        String username = principal.getClaim("sub");
+        return userApiService.getCurrentUser();
+    }
+
+    @PatchMapping("/currentUser")
+    public UserJson updateCurrentUser(@RequestBody UserJson user) {
+        return userApiService.updateCurrentUser(user);
+    }
+
+    @GetMapping("/friends")
+    public List<UserJson> getFriendsByUserId() {
+        return userApiService.getFriends();
+    }
+
+    @GetMapping("invitations")
+    public List<UserJson> getInvitations() {
+        return userApiService.getInvitations();
+    }
+
+    @PostMapping("users/invite/")
+    public UserJson sendInvitation(@RequestBody UserJson user) {
+        return userApiService.sendInvitation(user);
+    }
+
+    @PostMapping("friends/remove")
+    public UserJson removeFriendFromUser(@RequestBody UserJson friend) {
+        return userApiService.removeUserFromFriends(friend);
+    }
+
+    @PostMapping("friends/submit")
+    public UserJson submitFriend(@RequestBody UserJson friend) {
+        return userApiService.acceptInvitation(friend);
+    }
+
+    @PostMapping("friends/decline")
+    public UserJson declineFriend(@RequestBody UserJson friend) {
+        return userApiService.declineInvitation(friend);
+    }
+
+}
