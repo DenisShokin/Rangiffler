02b513b0d7240aab4112702c61e5b3bcc1ee10f7
Denis Shokin <DShokin@alfabank.ru>
2023-06-22 17:28:03 +0300

    step 3: rename photo, users application

--- rangiffler-photo/src/main/java/org/rangiffler/RangifflerPhotoApplication.java
+++ rangiffler-photo/src/main/java/org/rangiffler/RangifflerPhotoApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RangifflerUsersApp {
+public class RangifflerPhotoApplication {
     public static void main(String[] args) {
-        SpringApplication.run(RangifflerUsersApp.class, args);
+        SpringApplication.run(RangifflerPhotoApplication.class, args);
     }
 }
--- rangiffler-users/src/main/java/org/rangiffler/RangifflerUsersApplication.java
+++ rangiffler-users/src/main/java/org/rangiffler/RangifflerUsersApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RangifflerPhotoApp {
+public class RangifflerUsersApplication {
     public static void main(String[] args) {
-        SpringApplication.run(RangifflerPhotoApp.class, args);
+        SpringApplication.run(RangifflerUsersApplication.class, args);
     }
 }
